b59be62f0cb71416a0e1b44f928bc708523b6cc8
rd9911 <azizaziz9911@mail.ru>
2021-11-02 10:07:01 +0200

    Set up nodejs server, and  mongoose

--- blogs/blogs.js
+++ blogs/blogs.js
@@ -1,4 +1,33 @@
-let title = document.querySelector('.title').textContent
-let article = document.querySelector('.article').textContent
-let notes = document.querySelector('.notes').textContent
+const http = require('http');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
+main().catch(error => console.log(error))
+
+async function main () {
+    await mongoose.connect(`mongodb+srv://rd9911:${process.env.PASSWORD}@cluster0.sdvoq.mongodb.net/my-essays?retryWrites=true&w=majority`)
+    console.log('Connected')
+
+    const hostname = '127.0.0.1'
+    const port = 3000
+
+    const server = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.end()
+    })
+    server.listen(port, hostname, () => {
+        console.log(`Server is running at https://${hostname}/${port}`)
+    })
+}
+
+
+
+
+
+// let title = document.querySelector('.title').textContent
+// let article = document.querySelector('.article').textContent
+// let notes = document.querySelector('.notes').textContent
+
+// let blog = {title: title, article: article, notes: notes}
+// let blogs = []
