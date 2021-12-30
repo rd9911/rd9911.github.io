93e7a6a5cf2fd0a69604ab591a6b7b60c5289986
rd9911 <azizaziz9911@mail.ru>
2021-12-30 21:23:56 +0200

    Finish About me section.

--- blogs/pages/index.js
+++ blogs/pages/index.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import Image from 'next/image'
+import homer_image from '../public/main_content/homer.jpg'
 
 
 export default function Home() {
   return (
-    <div>
-      <div className='container'>
+      <div>
         <Head>
           <title>rd9911</title>
         </Head>
+        <div className='main-image'>
+          <Image src={homer_image} alt='welcoming image' />
+        </div>
       </div>
-    </div>
   )
 }
