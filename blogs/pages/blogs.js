d9b1f8e1ffc4edb2722e8766390c9b804daaa490
rd9911 <azizaziz9911@mail.ru>
2022-01-01 23:16:48 +0200

    Check.

--- blogs/pages/blogs.js
+++ blogs/pages/blogs.js
@@ -21,19 +21,19 @@ export default function Blogs({ posts }) {
     )
   }
   
-  export async function getStaticProps() {
-    const files = fs.readdirSync(path.join('posts'))
-    const posts = files.map(filename => {
-      const slug = filename.replace('.md', '')
-      const mdWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8') // get frontmatter
-      const { data: frontmatter } = matter(mdWithMeta)
-      return { slug, frontmatter }
-    })
-  
-  
-    return {
-      props: {
-        posts: posts.sort(sortByDate)
-      }
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+  const posts = files.map(filename => {
+    const slug = filename.replace('.md', '')
+    const mdWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8') // get frontmatter
+    const { data: frontmatter } = matter(mdWithMeta)
+    return { slug, frontmatter }
+  })
+
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate)
     }
   }
+}
