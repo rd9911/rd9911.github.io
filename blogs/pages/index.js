8e710dc15bb45891f74b45b85829f605d3602946
rd9911 <azizaziz9911@mail.ru>
2022-01-02 20:29:29 +0200

    Create buttons for social media links

--- blogs/pages/index.js
+++ blogs/pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import homer_image from '../public/main_content/homer.jpg'
+import github_image from '../public/main_content/github.png'
+import twitter_image from '../public/main_content/twitter.png'
+import linkedin_image from '../public/main_content/linkedin.png'
 
 
 export default function Home() {
@@ -15,6 +18,11 @@ export default function Home() {
           </div>
           <div className='intro-text'>
             <h1>I am Avaz, a full-stack developer in Helsinki, Finland who is specialized React.js and Node.js.</h1>
+            <div className='social-links'>
+              <a className='link' type='button' href='https://github.com/rd9911'><Image src={github_image} alt='github logo' width={58} height={30} /></a>
+              <a className='link' type='button' href='https://www.linkedin.com/in/avazjonyusufjonov1/'><Image src={linkedin_image} alt='github logo' width={30} height={30} /></a>
+              <a className='link' type='button' href='https://twitter.com/brodsky9911'><Image src={twitter_image} alt='github logo' width={35} height={35} /></a>
+            </div>
           </div>
         </div>
       </div>
