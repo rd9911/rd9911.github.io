909389f58a3b1b27260623f132b009681dbedee2
rd9911 <azizaziz9911@mail.ru>
2022-01-26 15:55:42 +0200

    Fix responsiveness for mobile.

--- blogs/pages/_app.js
+++ blogs/pages/_app.js
@@ -7,9 +7,9 @@ import Navbar from '../components/Navbar'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-     <meta name="viewport" content="width=device-width, initial-scale=2.0"/> 
       <Head>
           <title>rd9911</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
       </Head>
       <Header />
       <main>
